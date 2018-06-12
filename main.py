e28fee285575e69d2a0a12471de9d49a51af0c96
mrjones14 <christoph.rabl14@gmail.com>
2018-06-12 18:07:35 +0300

    add step 3 (player_input) and 4 (place_marker)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+# Step 1
+
 from IPython.display import clear_output
 
 def display_board(board):
@@ -15,6 +17,7 @@ def display_board(board):
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
 
+# Step 2
 
 def player_input():
     marker = ''
@@ -26,3 +29,24 @@ def player_input():
         return ('X', 'O')
     else:
         return ('O', 'X')
+# Step 3
+
+def place_marker(board, marker, position):
+    board[position] = marker
+    
+# Step 4
+
+def win_check(board, mark):
+    return(# ROWS:    
+    (board[1] == mark and board[2] == mark and board[3] == mark) or
+    (board[4] == mark and board[5] == mark and board[6] == mark) or
+    (board[7] == mark and board[8] == mark and board[9] == mark) or    
+    # COLUMS:    
+    (board[1] == mark and board[4] == mark and board[7] == mark) or
+    (board[2] == mark and board[5] == mark and board[8] == mark) or
+    (board[3] == mark and board[6] == mark and board[9] == mark) or
+    # DIAGONALS:
+    (board[1] == mark and board[5] == mark and board[9] == mark) or
+    (board[3] == mark and board[5] == mark and board[7] == mark))
+
+    
