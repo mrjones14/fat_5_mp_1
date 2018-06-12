3322d22b620d2e635a8c7fd15601f44bf23841b3
Roope <nykanen1@gmail.com>
2018-06-12 19:21:12 +0200

    abc

--- main.py
+++ main.py
@@ -44,20 +44,4 @@ def win_check(board, mark):
     (board[1] == mark and board[5] == mark and board[9] == mark) or
     (board[3] == mark and board[5] == mark and board[7] == mark))
 
-#step 7
-
-def full_board_check(board):
-    for i in range(1,10):
-        if space_check(board, i):
-            return False
-    return True
-
-#step 8
-
-def player_choice(board):
-    position = 0
     
-    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
-        position = int(input('Choose your next position: (1-9) '))
-        
-    return position
