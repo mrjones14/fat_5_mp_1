221c28a8695624c1b5eb262fc2cdb3cffb0b1ba7
mrjones14 <39957658+mrjones14@users.noreply.github.com>
2018-06-12 17:09:23 +0300

    Add files via upload

---
+++ mp1_step34.py
@@ -0,0 +1,16 @@
+# 
+def place_marker(board, marker, position):
+    board[position] = marker
+#
+def win_check(board, mark):
+    return(# ROWS:    
+    (board[1] == mark and board[2] == mark and board[3] == mark) or
+    (board[4] == mark and board[5] == mark and board[6] == mark) or
+    (board[7] == mark and board[8] == mark and board[9] == mark) or    
+    # COLUMS:    
+    (board[1] == mark and board[4] == mark and board[7] == mark) or
+    (board[2] == mark and board[5] == mark and board[8] == mark) or
+    (board[3] == mark and board[6] == mark and board[9] == mark) or
+    # DIAGONALS:
+    (board[1] == mark and board[5] == mark and board[9] == mark) or
+    (board[3] == mark and board[5] == mark and board[7] == mark))
