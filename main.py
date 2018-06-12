e512b556407d6eafbf55e8750882dd19e3c0847a
Jahaut <39926431+Jahaut@users.noreply.github.com>
2018-06-12 19:42:11 +0300

    removed conditionals

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ def choose_first():
     It chooses randomly which player goes first
     '''
     player = random.randint(1,2)
-    if player == 1:
-        print('Player 1 goes first!')
-    else:
-        print('Player 2 goes first!')
-
+    print(f'{player} goes first!')
 
 def space_check(board, position):
     '''
