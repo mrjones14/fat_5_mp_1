a412f47c119a36217a6e04e340cb0eddc442d5a7
Ilya Mikhalev <39466658+mikhily@users.noreply.github.com>
2018-06-12 17:07:03 +0300

    Step 1-2 Ilya

---
+++ MP1 Fat5 - Step 1+2  Ilya.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+from IPython.display import clear_output
+
+def display_board(board):
+    clear_output()  # Remember, this only works in jupyter!
+    
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+
+# In[8]:
+
+
+def player_input():
+    marker = ''
+    
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
